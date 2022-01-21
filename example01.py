53dc25aea624cd42e7f083c76bd21801fd91ac99
eanorambuena <eanorambuena@uc.cl>
2022-01-21 17:08:41 -0300

    test(Root): Delete second triangle. New requirement: GeneralBody Class

--- example01.py
+++ example01.py
@@ -1,8 +1,8 @@
-from xengine import * 
+from xengine import *
 from OpenGL.GL import * # pip install PyOpenGL
 from math import sqrt
 
-def setup(window: Window):
+def setup(window):
     a = 480
     h = a * sqrt(3) / 2
 
@@ -14,21 +14,7 @@ def setup(window: Window):
     T.adapt_to_window(window)
     T.draw()
 
-def loop(window: Window):
+def loop(window):
     glDrawArrays(GL_TRIANGLES, 0, 3)
 
 window = Window(720, 480, setup_function=setup, loop_function=loop)
-
-def setup(window: Window):
-    a = 0.5
-    h = a * sqrt(3) / 2
-
-    A = Point(-a/2, -h/2,   0, YELLOW)
-    B = Point(a/2,  -h/2,   0, MAGENTA)
-    C = Point(0,    h/2,    0, CIAN)
-
-    T = Triangle(A, B, C)
-    T.draw()
-
-window = Window(720, 480, setup_function=setup, loop_function=loop)
-
