bd2a55cf77b9611f5a7ff696735291c773742642
eanorambuena <eanorambuena@uc.cl>
2022-01-21 00:27:40 -0300

    test(Root): Simplify example with the new XEngine package interface classes

--- example01.py
+++ example01.py
@@ -1,1 +1,19 @@
-import xengine as xe
+from xengine import * 
+from OpenGL.GL import * # pip install PyOpenGL
+from math import sqrt
+
+def setup():
+    a = 1
+    h = a * sqrt(3) / 2
+
+    A = Point(-a/2, -h/2,   0, RED)
+    B = Point(a/2,  -h/2,   0, GREEN)
+    C = Point(0,    h/2,    0, BLUE)
+
+    V = Triangle(A, B, C)
+    V.draw()
+
+def loop():
+    glDrawArrays(GL_TRIANGLES, 0, 3)
+
+window = Window(720, 480, "XEngine Window", SETUP_FUNCTION=setup, LOOP_FUNCTION=loop)
