be9adc9001626c816ad18412215b4bca2a049e52
eanorambuena <eanorambuena@uc.cl>
2022-01-21 15:01:08 -0300

    test(Root): Update example with adapt_to_window method

--- example01.py
+++ example01.py
@@ -2,18 +2,19 @@ from xengine import *
 from OpenGL.GL import * # pip install PyOpenGL
 from math import sqrt
 
-def setup():
-    a = 1
+def setup(window):
+    a = 480
     h = a * sqrt(3) / 2
 
     A = Point(-a/2, -h/2,   0, RED)
     B = Point(a/2,  -h/2,   0, GREEN)
     C = Point(0,    h/2,    0, BLUE)
 
-    V = Triangle(A, B, C)
-    V.draw()
+    T = Triangle(A, B, C)
+    T.adapt_to_window(window)
+    T.draw()
 
-def loop():
+def loop(window):
     glDrawArrays(GL_TRIANGLES, 0, 3)
 
 window = Window(720, 480, setup_function=setup, loop_function=loop)
