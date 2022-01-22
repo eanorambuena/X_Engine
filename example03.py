07b52869442db585e1554531d721da32a317ec9a
eanorambuena <eanorambuena@uc.cl>
2022-01-21 21:58:42 -0300

    test(Root): Add example03

---
+++ example03.py
@@ -0,0 +1,24 @@
+from xengine import *
+from OpenGL.GL import * # pip install PyOpenGL
+
+from xengine.shaders import STANDARD_SHADER
+
+vertex_shader, fragment_shader = STANDARD_SHADER
+
+def setup(window):
+    a = 70
+    
+    A = Point(0,        0,      0, MAGENTA)
+    B = Point(4 * a,    0,      0, CIAN)
+    C = Point(0,        3 * a,  0, YELLOW)
+    D = Point(4 * a,    3 * a,  0, MAGENTA)
+    
+    T = GeneralShape(A, B, C, D)
+    T.adapt_to_window(window)
+    T.set_shader(vertex_shader, fragment_shader)
+    T.draw()
+
+def loop(window):
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4)
+
+window = Window(720, 480, setup_function=setup, loop_function=loop)
