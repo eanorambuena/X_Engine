678e73ffb472b149908f04d8fc875deabc88fb75
eanorambuena <eanorambuena@uc.cl>
2022-01-22 13:26:57 -0300

    feat(Root): Add zoom atribute to Point

--- xengine/points.py
+++ xengine/points.py
@@ -12,17 +12,23 @@ class Point(list):
 
         self.color = np.array([RGBA[0], RGBA[1], RGBA[2], RGBA[3]], dtype=np.float32)
 
+        self.zoom = UNDEFINED
+
     def adapt_to_window(self, window = UNDEFINED, width = UNDEFINED, height = UNDEFINED):
 
         if window is not UNDEFINED:
-            mid_width = window.width / 2
-            mid_height = window.height / 2
+            width = window.width
+            height = window.height
 
-        else:
-            mid_width = width / 2
-            mid_height = height / 2
+        mid_width = width / 2
+        mid_height = height / 2
 
-        self.vertices = np.array([self.x / mid_width, self.y / mid_height, self.z], dtype=np.float32)
+        ratio = mid_height / mid_width
+
+        if self.zoom is UNDEFINED:
+            self.zoom = 1 / mid_width
+        
+        self.vertices = np.array([self.x * ratio * self.zoom, self.y * self.zoom, self.z], dtype=np.float32)
 
     @property
     def x(self):
